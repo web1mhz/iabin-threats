84eb1812b99062f7816c4d37777cd2d8866d28e9
jhoanse <jhoanse@gmail.com>
2010-09-15 21:05:40 +0000

    Se integro el archivo properties, lectura de path  y de zooms

--- iabinmaps/src/tilecutter/TileCutter.java
+++ iabinmaps/src/tilecutter/TileCutter.java
@@ -5,23 +5,50 @@ import java.io.IOException;
 import tilecutter.raster.Raster;
 import tilecutter.tile.ImageManager;
 import tilecutter.tile.TileManager;
+import utils.PropertiesManager;
 
 /**
  * Clase principal (Main)
+ * 
  * @author supportadmin
- *
+ * 
  */
 public class TileCutter {
 
 	public static void main(String arg[]) throws IOException {
-		int zoom = Integer.parseInt(arg[0]);
-		Raster raster = new Raster();
-		raster.loadRaster(arg[1]);
-		ImageManager iManager = new ImageManager(arg[2]);
-		TileManager tManager = new TileManager(iManager);
+		// int zoom = Integer.parseInt(arg[0]);
+		PropertiesManager.register(arg[0]);
 		
-		tManager.cutRaster(raster, zoom);
+		String[] rastersID = PropertiesManager.getInstance().getPropertiesAsStringArray("rasters");
+		Raster raster;
+		ImageManager iManager;
 		
+		String targetPath = PropertiesManager.getInstance().getPropertiesAsString("path.target");
+		String sourcePath = PropertiesManager.getInstance().getPropertiesAsString("path.source");
 		
+		for (String rasterID : rastersID) {
+			// raster.loadRaster(arg[1]);
+			// ImageManager iManager = new ImageManager(arg[2]);
+			raster = new Raster();
+			
+			String group = PropertiesManager.getInstance().getPropertiesAsString(rasterID+".group");
+			String pathGroup = PropertiesManager.getInstance().getPropertiesAsString(group+".path");
+			String fileName = PropertiesManager.getInstance().getPropertiesAsString(rasterID+".filename");
+			
+			
+			iManager = new ImageManager(targetPath+pathGroup+rasterID);
+			
+			TileManager tManager = new TileManager(iManager);
+
+			raster.loadRaster(sourcePath+pathGroup+fileName);
+
+			int zoomMin = PropertiesManager.getInstance().getPropertiesAsInt("zoom.min");
+			int zoomMax = PropertiesManager.getInstance().getPropertiesAsInt("zoom.max");
+			while (zoomMin <= zoomMax) {
+				tManager.cutRaster(raster, zoomMin);
+				zoomMin++;
+			}
+		}
+
 	}
 }
