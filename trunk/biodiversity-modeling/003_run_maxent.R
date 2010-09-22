a4df6fcd841820b23ff6bca09120b94f961274b3
j.m.signer <j.m.signer@ffa86d28-1ee6-fafb-8203-f7d4c9a408cd>
2010-09-22 19:27:45 +0000

    added snowfall

--- trunk/biodiversity-modeling/003_run_maxent.R
+++ trunk/biodiversity-modeling/003_run_maxent.R
@@ -28,7 +28,7 @@ dir.create(paste(sp_id,"/cross", sep=""))
 dir.create(paste(sp_id,"/proj", sep=""))
 
 # train the model
-system(paste("java -mx",max.ram,"m -jar ", dir.maxent, "/maxent.jar nowarnings outputdirectory=", sp_id, "/results samplesfile=",sp_id,"/training/species_swd.csv environmentallayers=",i,"/training/background_swd.csv -a -z", sep=""))
+system(paste("java -mx",max.ram,"m -jar ", dir.maxent, "/maxent.jar nowarnings outputdirectory=", sp_id, "/results samplesfile=",sp_id,"/training/species_swd.csv environmentallayers=",sp_id,"/training/background_swd.csv -a -z", sep=""))
 					
 # crossvalidate
 system(paste("java -mx",max.ram,"m -jar ", dir.maxent, "/maxent.jar nowarnings outputdirectory=", sp_id, "/cross samplesfile=",sp_id,"/training/species_swd.csv environmentallayers=",sp_id,"/training/background_swd.csv -P replicates=",no.replicates," replicatetype=",replicate.type," -a -z", sep=""))
@@ -36,11 +36,16 @@ system(paste("java -mx",max.ram,"m -jar ", dir.maxent, "/maxent.jar nowarnings o
 # project
 system(paste("java -mx",max.ram,"m -cp ", dir.maxent, "/maxent.jar density.Project ",sp_id,"/results/",sp_id,".lambdas ",dir.proj, " ",sp_id,"/proj/",sp_id," nowarnings fadebyclamping -r -a -z", sep=""))
 
+
+## zipWrite fails with snowfall, hence use the unix commands
+system(paste("tar -zcvf ",sp_id,".tar.gz ", sp_id,sep=""))
+system(paste("rm -rv",sp_id))
+
 ## write *.gz, function provided by J. Ramirez
-for (raster in list.files(paste(sp_id,"proj/", sep="/"))) zipWrite(raster(paste(sp_id,"proj",raster, sep="/")), paste(sp_id,"proj", sep="/"), paste(strsplit(raster, "\\.")[[1]][1],"gz", sep="."))
+#for (raster in list.files(paste(sp_id,"proj/", sep="/"))) zipWrite(raster(paste(sp_id,"proj",raster, sep="/")), paste(sp_id,"proj", sep="/"), paste(strsplit(raster, "\\.")[[1]][1],"gz", sep="."))
 
 # rm original files
-file.remove(paste(sp_id,"/proj/",list.files(paste(sp_id,"/proj/", sep=""), pattern="*.asc"), sep=""))
+#file.remove(paste(sp_id,"/proj/",list.files(paste(sp_id,"/proj/", sep=""), pattern="*.asc"), sep=""))
 
 }
 
