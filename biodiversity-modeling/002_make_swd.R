623e411954a6a8d6233dd52b8fa520a75ed5e46f
j.m.signer <j.m.signer@ffa86d28-1ee6-fafb-8203-f7d4c9a408cd>
2010-09-22 19:27:45 +0000

    added snowfall

--- biodiversity-modeling/002_make_swd.R
+++ biodiversity-modeling/002_make_swd.R
@@ -12,8 +12,6 @@
 # ecoregions: raster with eco-regions for SA, z-value=Biome
 # v.all: all values from ecoregions (getValues(ecoregions)), when supplied, time is saved
 # n.background: number of background points
-# dir.maxent: directory where maxent is found 
-
 
 # return
 #
@@ -22,10 +20,14 @@
 # Script: no changes should be made below #
 ###########################################
 
-make.swd <- function(sp_id,points, ecoregions, v.all="NA", no.background=10000, dir.maxent, env.var)
-{
-  if (v.all=="NA") v.all <- getValues(ecoregions) # save time, when doing it only once, so better to pass as argument
 
+get.background <- function(sp_id, ecoregions, v.all, no.background=10000)
+{
+  points <- read.csv(paste(sp_id,"/training/species.csv", sep=""))
+
+  if (nrow(points) > 40) {env.var <- env.full  
+  } else env.var <- env.reduced  
+  
   v.ok <- unique(xyValues(ecoregions, points[,2:3]))
   #v.all <- getValues(ecoregions)
   v.ok <- v.ok[!is.na(v.ok)]
@@ -36,8 +38,10 @@ make.swd <- function(sp_id,points, ecoregions, v.all="NA", no.background=10000, 
   write.table("species,longitude,latitude",paste(sp_id,"/training/background.csv", sep=""),col.names=F, row.names=F,append=F, quote=F)
   write.table(c("background", background), paste(sp_id,"/training/background.csv",sep=""), col.names=F, row.names=F, sep=",", append=T, quote=F)
 
+  # write env variables
+   write(paste("env.var : ",env.var, sep=""), paste(sp_id,"info.txt", sep="/"), append=T)
   # make swd.species
-  system(paste("java -cp ", dir.maxent, "/maxent.jar density.Getval ./",sp_id,"/training/species.csv ",env.var, ">",sp_id,"/training/species_swd.csv", sep=""))
+  system(paste("java -cp ", dir.maxent, "/maxent.jar density.Getval ",sp_id,"/training/species.csv ",env.var, ">",sp_id,"/training/species_swd.csv", sep=""))
   # make swd background
-  system(paste("java -cp ", dir.maxent, "/maxent.jar density.Getval ./",sp_id,"/training/background.csv ",env.var, ">",sp_id,"/training/background_swd.csv", sep=""))
+  system(paste("java -cp ", dir.maxent, "/maxent.jar density.Getval ",sp_id,"/training/background.csv ",env.var, ">",sp_id,"/training/background_swd.csv", sep=""))
 }
