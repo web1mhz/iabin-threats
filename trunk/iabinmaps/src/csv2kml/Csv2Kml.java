fba6e65c3952a81dd24c050770685e4ba2adbc02
lotvxx@gmail.com <lotvxx@gmail.com@ffa86d28-1ee6-fafb-8203-f7d4c9a408cd>
2010-09-29 16:10:10 +0000

    csv to polygon conversion, with inner and outer boundaries, added

--- trunk/iabinmaps/src/csv2kml/Csv2Kml.java
+++ trunk/iabinmaps/src/csv2kml/Csv2Kml.java
@@ -3,28 +3,44 @@ package csv2kml;
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
 
+import utils.PropertiesManager;
+
 public class Csv2Kml {
 
 
 	static LinkedList<String[]> lista;
+	static LinkedList<String[]> listaChull;
+	static LinkedList<String[]> listaChullBuff;
+	public static Csv2Kml c2k;
+	
 	
 	public static void main(String[] args) {
 		
+		String estilo="http://wikipedia.agilityhoster.com/estilo.kml#estilo";//se debe agregar properties
 		
 		CsvFile file=new CsvFile("d:/csv/101616.csv");
 
 		lista=file.getLista();
 		System.out.println(lista.getFirst());
 		
-		Csv2Point point=new Csv2Point(lista);
+		CsvFile chull=new CsvFile("d:/csv/101616-chull.csv");
+		listaChull=chull.getLista();
+		CsvFile chullbuff=new CsvFile("d:/csv/101616-chull-buff.csv");
+		listaChullBuff=chullbuff.getLista();
+		
+		
+		
+		Csv2Point point=new Csv2Point(lista);   
 		try {
 			point.createKML("d:");
-		} catch (FileNotFoundException e) {
-			
-			e.printStackTrace();
-		}
-		Csv2Polygon pol=new Csv2Polygon();
+		} catch (FileNotFoundException e) {		e.printStackTrace();		}
 		
+		
+		
+		Csv2Polygon pol=new Csv2Polygon(listaChull,listaChullBuff, estilo);
+		try {
+			pol.createKML("d:");
+		} catch (FileNotFoundException e) {		e.printStackTrace();		}
 
 	}
 
--- trunk/iabinmaps/src/csv2kml/Csv2Polygon.java
+++ trunk/iabinmaps/src/csv2kml/Csv2Polygon.java
@@ -1,17 +1,63 @@
 package csv2kml;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import de.micromata.opengis.kml.v_2_2_0.Boundary;
+import de.micromata.opengis.kml.v_2_2_0.Coordinate;
+import de.micromata.opengis.kml.v_2_2_0.Kml;
+import de.micromata.opengis.kml.v_2_2_0.KmlFactory;
+import de.micromata.opengis.kml.v_2_2_0.LinearRing;
+import de.micromata.opengis.kml.v_2_2_0.Placemark;
+import de.micromata.opengis.kml.v_2_2_0.Polygon;
+
 public class Csv2Polygon {
 
-	
-	public void createInner(CsvFile file){
-		
+	private LinkedList<String[]> listaChull;
+	private LinkedList<String[]> listaChullBuff;
+	private String estilo;
+
+	public Csv2Polygon(LinkedList<String[]> listaChull,
+			LinkedList<String[]> listaChullBuff, String estilo) {
+
+		this.estilo = estilo;
+		this.listaChull = listaChull;
+		this.listaChullBuff = listaChullBuff;
+
 	}
-	
-	public void createOuter(CsvFile file){
-		
+
+	public void createKML(String path) throws FileNotFoundException {
+
+		String filename = "d:/poligono.kml";
+		Placemark placemark = KmlFactory.createPlacemark().withName(("Name"))
+				.withDescription("descripcion").withStyleUrl(estilo);
+
+		Kml kml = new Kml();
+
+		Polygon pol = placemark.createAndSetPolygon();
+		final Boundary bound = new Boundary();
+		final LinearRing lin = bound.createAndSetLinearRing();
+		List<Coordinate> kmlCoords = lin.createAndSetCoordinates();
+
+		for (String[] s : listaChull) {
+			Coordinate coord = new Coordinate(Double.parseDouble(s[0]), Double.parseDouble(s[1]));
+			kmlCoords.add(coord);
+		}
+		pol.setOuterBoundaryIs(bound);
+
+		kmlCoords = lin.createAndSetCoordinates();
+		for (String[] s : listaChullBuff) {
+			Coordinate coord = new Coordinate(Double.parseDouble(s[0]), Double.parseDouble(s[1]));
+			kmlCoords.add(coord);
+		}
+		pol.addToInnerBoundaryIs(bound);
+
+		kml.setFeature(placemark);// se registra el folder al kml
+
+		kml.marshal();// se imprime kml en consola
+		kml.marshal(new File(filename));// se guarda kml en archivo
+
 	}
-	
-	public void write(){
-		
-	}
+
 }
