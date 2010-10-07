0fd6c0161e332d28147c9b6824808ffb96dc2ea6
j.m.signer <j.m.signer@ffa86d28-1ee6-fafb-8203-f7d4c9a408cd>
2010-10-07 17:46:55 +0000

    removed snowfall again

--- trunk/biodiversity-modeling/001_write_species_csv.R
+++ trunk/biodiversity-modeling/001_write_species_csv.R
@@ -2,7 +2,7 @@
 # Author: Johannes Signer
 # Contact: j.m.signer@gmail.com
 # Date: 10.9.2010
-# Purpose: Creat csv files for each species
+#p Purpose: Creat csv files for each species
 # Comments: 
 ###########################################
 ############ Preliminaries ################
@@ -26,41 +26,46 @@
 ###########################################
 
 
-write.species.csv <- function(records, req.points=10, log.file="log.txt")
+write.species.csv <- function(x, req.points=10, log.file="log.txt", dir.out=dir.out)
 {
+   # all records    
+   records <- read.csv(x[1])
+   sp.class <- x[2]
+   # vector that returns all processed species ids
+   sp.processed <- c()
+   # create 'primary key' based on locations
+   records$loc.key <- paste(records$lat,records$lon, sep="")
 
-# create 'primary key' based on locations
-records$loc.key <- paste(records$lat,records$lon, sep="")
+   for (i in unique(records$specie_id))
+   {
+      this.sp <- records[records$specie_id==i,]
+      this.sp.u <- this.sp[!duplicated(this.sp$loc.key),]
 
-for (i in unique(records$specie_id))
-{
-  this.sp <- records[records$specie_id==i,]
-  this.sp.u <- this.sp[!duplicated(this.sp$loc.key),]
-    
+      if(nrow(this.sp.u)>=req.points)
+      {
+          # create file
+         dir.create(paste(dir.out,i,"training",sep="/"),recursive=T)
 
-  if(nrow(this.sp.u)>=req.points)
-  {
-    # create file
-   if(!file.exists(paste(i))) dir.create(paste(i))
-   if(!file.exists(paste(i,"/training",sep=""))) dir.create(paste(i,"/training",sep=""))
-   write(paste("species id : ",i,
+         write(paste("species id : ",i,
               "\nspecie : ",this.sp.u[1,'specie'],
               "\ngenus : ",this.sp.u[1,'genus'],
               "\ngenus_id : ",this.sp.u[1,'genus_id'],
               "\nfamily_id : ",this.sp.u[1,'family_id'], 
               "\nfamily : ", this.sp.u[1,'family'],
-              "\nclass : ",this.sp.u[1,'class'],
+              "\nclass : ",sp.class,
               "\nfile.created : ",date(), 
-              "\nnumber.of.points : ",nrow(this.sp.u), sep=""), paste(i,"info.txt", sep="/"))
+              "\nnumber.of.points : ",nrow(this.sp.u), sep=""), paste(dir.out,i,"info.txt", sep="/"))
   
-    write.table("species,lon,lat", paste(i,"/training/species.csv", sep=""), row.names=F, col.names=F, quote=F, sep=",", append=F)
-    write.table(this.sp.u[,c("specie_id","lon","lat")], paste(i,"/training/species.csv", sep=""), row.names=F, col.names=F, quote=F, sep=",", append=T)
+         write.table("species,lon,lat", paste(dir.out,i,"training/species.csv", sep="/"), row.names=F, col.names=F, quote=F, sep=",", append=F)
+         write.table(this.sp.u[,c("specie_id","lon","lat")], paste(dir.out,i,"training/species.csv", sep="/"), row.names=F, col.names=F, quote=F, sep=",", append=T)
 
-    # write to the log file
-    write(paste(date(),i,nrow(this.sp.u),"yes", sep=","), log.file, append=T)
-  } else write(paste(date(),i,nrow(this.sp.u),"no", sep=","), log.file, append=T)
+      # write to the log file
+      write(paste(date(),i,nrow(this.sp.u),"yes", sep=","), log.file, append=T)
+      # species to the list of processed species
+      sp.processed <- c(sp.processed,i)
+   } else write(paste(date(),i,nrow(this.sp.u),"no", sep=","), log.file, append=T)
 
-}
-print(paste("completed ", sep=""))
+}  
+return(sp.processed)
 }
    
