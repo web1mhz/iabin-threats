1378fc03e5f755011deb3f7af5a104b0885a9e9d
daniel.amariles88 <daniel.amariles88@ffa86d28-1ee6-fafb-8203-f7d4c9a408cd>
2010-10-25 15:36:42 +0000

    commiting queries

---
+++ trunk/ita/queries/QUERY_drop_temporal_tables.sql
@@ -0,0 +1,14 @@
+--after step 1
+DROP TABLE IF EXISTS temp_land_1A;
+DROP TABLE IF EXISTS temp_land_2A;
+DROP TABLE IF EXISTS temp_land_3A;
+--after step 2  
+DROP TABLE IF EXISTS temp_land_4A;
+--after step 3
+DROP TABLE IF EXISTS temp_land_5A;
+--after step 6
+DROP TABLE IF EXISTS temp_good_records;
+DROP TABLE IF EXISTS temp_bad_records;
+DROP TABLE IF EXISTS temp_good_1A;
+DROP TABLE IF EXISTS temp_good_2A;
+DROP TABLE IF EXISTS temp_good_3A;
