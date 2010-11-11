a50050b76b2831a8e10458667411ca766b8216e8
daniel.amariles88 <daniel.amariles88@ffa86d28-1ee6-fafb-8203-f7d4c9a408cd>
2010-11-11 17:28:08 +0000

    getPointForMarker method

--- trunk/ita-maps/src/main/java/org/ciat/ita/maps/shape2kml/shape/Shapefile.java
+++ trunk/ita-maps/src/main/java/org/ciat/ita/maps/shape2kml/shape/Shapefile.java
@@ -14,7 +14,11 @@ import org.geotools.feature.FeatureIterator;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
-
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
 
 public class Shapefile {
 
@@ -71,4 +75,60 @@ public class Shapefile {
 	public ReferencedEnvelope collectionLimits() {
 		return collection.getBounds();
 	}
+	
+	/**
+	 * Obtain the ideal point to place a marker in a polygon
+	 * @param sf is the simple feature to get the marker point
+	 * @return
+	 */
+	public static Point getPointForMarker(SimpleFeature sf){
+			
+		MultiPolygon mp;
+		Point centroid;
+		double distance;
+		if(!(sf.getDefaultGeometry() instanceof MultiPolygon)){
+			return null;
+		}
+		mp=(MultiPolygon) sf.getDefaultGeometry();
+		centroid=mp.getCentroid();
+		distance=mp.distance(centroid);
+		if(distance==0){
+			return centroid;
+		}
+			
+		double difX=sf.getBounds().getMaxX()-sf.getBounds().getMinX();
+		double difY=sf.getBounds().getMaxY()-sf.getBounds().getMinY();
+		GeometryFactory gf = new GeometryFactory();
+		LineString in;
+			
+		/* get the point from the center of the narrowest part of the polygon */
+		boolean byX=difX<difY;
+		if(byX){
+			in= gf.createLineString(new Coordinate[]{
+					new Coordinate(sf.getBounds().getMinX()+difX, 90),
+					new Coordinate(sf.getBounds().getMinX()+difX, -90)});
+		}else{
+			in= gf.createLineString(new Coordinate[]{
+					new Coordinate(180, sf.getBounds().getMinY()+difY),
+					new Coordinate(-180, sf.getBounds().getMinY()+difY)});
+		}
+		
+		centroid=mp.intersection(in).getCentroid();
+		if(centroid!=null && mp.distance(centroid)==0){
+				return centroid;
+			}
+		/* find a random point inside the polygon */
+		while(distance>0){
+			Point pointPol = gf.createPoint(new Coordinate(sf.getBounds().getMinX()+(Math.random()*difX)
+					, sf.getBounds().getMinY()+(Math.random()*difY)));
+			distance=mp.distance(pointPol);
+			if(distance==0){
+				return pointPol;
+			}
+				
+		}
+	
+		return null;
+	}
+	
 }
