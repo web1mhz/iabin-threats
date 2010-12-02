aaf93f2c4152bc755276671d3506bdec63f3325c
lotvxx@gmail.com <lotvxx@gmail.com@ffa86d28-1ee6-fafb-8203-f7d4c9a408cd>
2010-12-02 18:59:24 +0000

    Initial import.

---
+++ trunk/ita-bg/src/main/java/org/ciat/ita/gb/App.java
@@ -0,0 +1,13 @@
+package org.ciat.ita.gb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
