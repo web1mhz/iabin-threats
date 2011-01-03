b9c29c5d5b11d92181c62579bc0cd993a8b8fccb
jhoanse@gmail.com <jhoanse@gmail.com@ffa86d28-1ee6-fafb-8203-f7d4c9a408cd>
2011-01-03 13:54:40 +0000

    Cambios en la escala de la imagen

--- trunk/ita-maps/src/main/java/org/ciat/ita/maps/tilecutter/tile/colormanager/ContinousColorManager.java
+++ trunk/ita-maps/src/main/java/org/ciat/ita/maps/tilecutter/tile/colormanager/ContinousColorManager.java
@@ -1,6 +1,7 @@
 package org.ciat.ita.maps.tilecutter.tile.colormanager;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GradientPaint;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
@@ -56,8 +57,8 @@ public class ContinousColorManager extends ColorManager{
 	  
 		int line = 35;
 		
-		int recWidth = 40;
-		int recHeight = line*4-recWidth;
+		int recWidth = 60;
+		int recHeight = line*4-recWidth+20;
 		
 		int width = recWidth*5;
 		int height = line*4+10;
@@ -69,20 +70,24 @@ public class ContinousColorManager extends ColorManager{
 		
 		graphics.setColor(Color.WHITE);
 		graphics.fillRect(0, 0, width, height);
-						
+		
+		 int fontSize = 12;
+		Font font = new Font("Arial", Font.PLAIN, fontSize);
+		    
+		graphics.setFont(font);
 		graphics.setColor(Color.BLACK);
 		graphics.drawString(descripcion,5,(line-recHeight)/2+recHeight/2);
 		
 		Color colorMin = new Color(getRGB(getMin()));
 		Color colorMax = new Color(getRGB(getMax()));
 		graphics.setPaint(new GradientPaint(5, (line-recHeight)/2+line*2, colorMax,0, recHeight+line, colorMin ));
-		graphics.fillRect(5,(line-recHeight)/2+line*2, recWidth, recHeight);
+		graphics.fillRect(5,(line-recHeight)/2+line*2, recWidth-20, recHeight);
 		
 		
 		graphics.setColor(Color.BLACK);
-		graphics.drawRect(5,(line-recHeight)/2+line*2, recWidth, recHeight);
-		graphics.drawString("High: "+Float.toString(this.getMax()/10), recWidth+10,(line-recHeight)/2+recHeight/2+30);
-		graphics.drawString("Low: "+Float.toString(this.getMin()/10), recWidth+10,recHeight+40);
+		graphics.drawRect(5,(line-recHeight)/2+line*2, recWidth-20, recHeight);
+		graphics.drawString("High: "+Float.toString(this.getMax()/10), recWidth-5,(line-recHeight)/2+recHeight/2+30);
+		graphics.drawString("Low: "+Float.toString(this.getMin()/10), recWidth-5,recHeight+40);
 		
 		
 		return image;
