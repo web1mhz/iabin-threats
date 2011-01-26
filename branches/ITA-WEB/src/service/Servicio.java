bda3c29542f5902e69ee50f9ad89f00fbb05663a
jhoanse@gmail.com <jhoanse@gmail.com@ffa86d28-1ee6-fafb-8203-f7d4c9a408cd>
2011-01-26 21:27:30 +0000

    agregando libreria mysql-connector

--- branches/ITA-WEB/src/service/Servicio.java
+++ branches/ITA-WEB/src/service/Servicio.java
@@ -12,52 +12,54 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.DataBaseManager;
 
-
 public class Servicio extends HttpServlet {
 	private static final long serialVersionUID = 7332571365625168689L;
-	private String user="jacamacho";
-	private String pass="123456";
-	private String ip="gisbif.ciat.cgiar.org";
-	private String port="3306";
-	private String database="iabin_sstn";
+	private String user = "jacamacho";
+	private String pass = "123456";
+	private String ip = "gisbif.ciat.cgiar.org";
+	private String port = "3306";
+	private String database = "iabin_sstn";
 	private Connection conx;
 
 	@Override
 	public void init() throws ServletException {
 		super.init();
-		
+
 		DataBaseManager.registerDriver();
-		conx = DataBaseManager.openConnection(user, pass,ip, port, database);
+		conx = DataBaseManager.openConnection(user, pass, ip, port, database);
 		/*
-		 * Aquí irá todo lo relacionado con la creación e inicialización de la conexión a la base de datos.
-		 * Hay que tener en cuenta que la conexión se puede perder en cualquier momento por
-		 * falta de uso de los usuarios (que nadie entre) y porque ya se cumplió el timeout del servidor
-		 * de base de datos de mysql, por tal motivo hay que estar pendiente de ello y
-		 * reiniciar la conexión en caso de que lo requiera.
+		 * Aquí irá todo lo relacionado con la creación e inicialización de la
+		 * conexión a la base de datos. Hay que tener en cuenta que la conexión
+		 * se puede perder en cualquier momento por falta de uso de los usuarios
+		 * (que nadie entre) y porque ya se cumplió el timeout del servidor de
+		 * base de datos de mysql, por tal motivo hay que estar pendiente de
+		 * ello y reiniciar la conexión en caso de que lo requiera.
 		 */
 	}
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
-		boolean respuesta=false;
-		
+
+		boolean respuesta = false;
+
 		String id = req.getParameter("id");
-		
-		respuesta=consulta(id); //se hace la consulta y se devuelven los objetos
-		if(respuesta) System.out.println("se realizo la consulta");
-		
+
+		respuesta = consulta(id); // se hace la consulta y se devuelven los
+									// objetos
+		if (respuesta)
+			System.out.println("se realizo la consulta");
+
 		resp.setContentType("text/html");
-		//if(id != null && id.equals("10")) {
+		// if(id != null && id.equals("10")) {
 		PrintWriter out = resp.getWriter();
-		out.print("Hola Mundo - "+id);
+		out.print("Hola Mundo - " + id);
 		out.flush();
 		out.close();
-			
-		//}
+
+		// }
 	}
-	
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
@@ -69,17 +71,17 @@ public class Servicio extends HttpServlet {
 		// TODO Auto-generated method stub
 		super.destroy();
 	}
-	
-	
-	public boolean consulta(String id){
 
-		ResultSet rs = DataBaseManager.makeQuery("select * from georeferenced_records where id="+id, conx);
-		DataBaseManager.closeConnection(conx);
+	public boolean consulta(String id) {
 
-		//se supone que debe devolver el hashset de los objetos que se necesitan 
+		System.out.println("id : " + id);
+		ResultSet rs = DataBaseManager.makeQuery(
+				"select * from georeferenced_records where id=" + id, conx);
+		
+
+		// se supone que debe devolver el hashset de los objetos que se
+		// necesitan
 		return true;
 	}
-	
 
-	
 }
