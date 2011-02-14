88cfd4013da1f7883ec52b9f4b522d702215ffbc
jhoanse <jhoanse@ffa86d28-1ee6-fafb-8203-f7d4c9a408cd>
2011-02-14 13:47:07 +0000

    commiting files... (Project not working yet).. errores still appearing.

---
+++ trunk/ita-wt/src/main/java/org/ciat/ita/model/TaxonObject.java
@@ -0,0 +1,47 @@
+package org.ciat.ita.model;
+
+public class TaxonObject implements Comparable<TaxonObject> {
+	private String id;
+	private String canonical;
+	private int rankID;	
+	
+	public TaxonObject() {
+		super();
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCanonical() {
+		return canonical;
+	}
+	public void setCanonical(String canonical) {
+		this.canonical = canonical;
+	}
+	public int getRankID() {
+		return rankID;
+	}
+	public void setRankID(int rankID) {
+		this.rankID = rankID;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof TaxonObject) {
+			TaxonObject to = (TaxonObject) obj;
+			return to.id.equals(this.getId());
+		}
+		return false;
+	}
+	@Override
+	public int hashCode() {		
+		return id.hashCode();
+	}
+	@Override
+	public int compareTo(TaxonObject o) {
+		
+		return (this.getCanonical()+this.getRankID()).compareTo(o.getCanonical()+o.getRankID());
+	}
+	
+}
---
+++ trunk/ita-wt/src/main/java/org/ciat/ita/service/Info.java
@@ -0,0 +1,29 @@
+package org.ciat.ita.service;
+
+public class Info {
+
+	private static String user = "guest";
+	private static String pass = "cajanus";
+	private static String ip = "gisbif.ciat.cgiar.org";
+	private static String port = "3306";
+	private static String database = "iabin_sstn";
+	
+	
+	public static String getUser() {
+		return user;
+	} 
+	public static String getPass() {
+		return pass;
+	}
+	public static String getIp() {
+		return ip;
+	}
+	public static String getPort() {
+		return port;
+	}
+	public static String getDatabase() {
+		return database;
+	}
+	
+	
+}
