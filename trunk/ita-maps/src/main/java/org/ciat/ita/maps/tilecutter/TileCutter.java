3886dcc590886f7c076546661263e974abd04b0a
lotvxx@gmail.com <lotvxx@gmail.com@ffa86d28-1ee6-fafb-8203-f7d4c9a408cd>
2011-02-23 14:33:08 +0000

    added menu 5 - species distribution- converts each asc file in folder to tile image

--- trunk/ita-maps/src/main/java/org/ciat/ita/maps/tilecutter/TileCutter.java
+++ trunk/ita-maps/src/main/java/org/ciat/ita/maps/tilecutter/TileCutter.java
@@ -39,37 +39,27 @@ public class TileCutter {
 
 		if(control==true) PropertiesManager.register(arg);
 
-		String[] rastersID = PropertiesManager.getInstance()
-				.getPropertiesAsStringArray("rasters");
+		String[] rastersID = PropertiesManager.getInstance().getPropertiesAsStringArray("rasters");
 		Raster raster;
 		ImageManager iManager;
 
-		String targetPath = PropertiesManager.getInstance()
-				.getPropertiesAsString("path.target");
-		String sourcePath = PropertiesManager.getInstance()
-				.getPropertiesAsString("path.source");
+		String targetPath = PropertiesManager.getInstance().getPropertiesAsString("path.target");
+		String sourcePath = PropertiesManager.getInstance().getPropertiesAsString("path.source");
+		
+		
 
 		for (String rasterID : rastersID) {
-
-			 raster = new org.ciat.ita.maps.tilecutter.raster.Raster();
-			String descripcion = PropertiesManager.getInstance()
-					.getPropertiesAsString(rasterID + ".descripcion");
-			String group = PropertiesManager.getInstance()
-					.getPropertiesAsString(rasterID + ".group");
-			String pathGroup = PropertiesManager.getInstance()
-					.getPropertiesAsString(group + ".path");
-			String fileName = PropertiesManager.getInstance()
-					.getPropertiesAsString(rasterID + ".filename");
+			raster = new org.ciat.ita.maps.tilecutter.raster.Raster();
+			 
+			String descripcion = PropertiesManager.getInstance().getPropertiesAsString(rasterID + ".descripcion");
+			String group = PropertiesManager.getInstance().getPropertiesAsString(rasterID + ".group");
+			String pathGroup = PropertiesManager.getInstance().getPropertiesAsString(group + ".path");
+			String fileName = PropertiesManager.getInstance().getPropertiesAsString(rasterID + ".filename");
 
 			iManager = new ImageManager(targetPath + pathGroup + rasterID);
-
 			TileManager tManager = new TileManager(iManager);
-
 			raster.loadRaster(sourcePath + pathGroup + fileName);
-
-			ColorManager cManager = ColorManagerFactory.createColorManager(
-					rasterID, raster);
-
+			ColorManager cManager = ColorManagerFactory.createColorManager(rasterID, raster);
 			iManager.setColorManager(cManager);
 
 			BufferedImage scaleImage = cManager.getScaleImage(descripcion);
@@ -77,24 +67,70 @@ public class TileCutter {
 			File dir = new File(targetPath+ pathGroup + rasterID + File.separator);
 			dir.mkdirs();
 			try {
-				ImageIO.write(scaleImage, "png", new File(targetPath
-						+ pathGroup + rasterID + File.separator + rasterID
-						+ "scaleTestImage.png"));
-			} catch (IOException e) {
-				e.printStackTrace();
+				ImageIO.write(scaleImage, "png", new File(targetPath+ pathGroup + rasterID + File.separator + rasterID+ "scaleTestImage.png"));
+			} catch (IOException e) { e.printStackTrace();
 			}
-
-			int zoomMin = PropertiesManager.getInstance().getPropertiesAsInt(
-					"zoom.min");
-			int zoomMax = PropertiesManager.getInstance().getPropertiesAsInt(
-					"zoom.max");
+			int zoomMin = PropertiesManager.getInstance().getPropertiesAsInt("zoom.min");
+			int zoomMax = PropertiesManager.getInstance().getPropertiesAsInt("zoom.max");
 			while (zoomMin <= zoomMax) {
 				tManager.cutRaster(raster, zoomMin);
 				zoomMin++;
 			}
-		}
+		}//fin for
+		
+		
+
+		
 
 	
+	}
+	
+	//converts the raster (.asc) files in the folder specified in properties file, to tile images
+	public static void createSpeciesDistributionImages(String arg) throws IOException{
+		
+		if(control==true) PropertiesManager.register(arg);
+
+		Raster raster;
+		ImageManager iManager;
+
+		String targetPath = PropertiesManager.getInstance().getPropertiesAsString("path.target");
+		String sourcePath = PropertiesManager.getInstance().getPropertiesAsString("path.source");
+		String species = PropertiesManager.getInstance().getPropertiesAsString("species.path");
+		String speciesDistribution = PropertiesManager.getInstance().getPropertiesAsString("speciesDistribution");
+		if (speciesDistribution.equalsIgnoreCase("true")) System.out.println("se recorrerá la carpeta de especies");
+
+		
+		File folder = new File(sourcePath + species);
+		System.out.println("folder: "+folder);
+		File[] listOfFiles = folder.listFiles();
+		for (File s : listOfFiles) {
+			if (s.isDirectory()) {
+		
+			String rasterID="speciesDistribution";
+			raster = new org.ciat.ita.maps.tilecutter.raster.Raster();
+			 
+			String group = PropertiesManager.getInstance().getPropertiesAsString(rasterID + ".group");
+			String pathGroup = PropertiesManager.getInstance().getPropertiesAsString(group + ".path");
+			String fileName = (s.getName()+File.separator + s.getName()+"_full_with_threshold.asc");
+			System.out.println("filename: "+fileName);
+			
+			iManager = new ImageManager(targetPath + pathGroup + s.getName());
+			TileManager tManager = new TileManager(iManager);
+			raster.loadRaster(sourcePath + pathGroup + fileName);
+			ColorManager cManager = ColorManagerFactory.createColorManager(rasterID, raster);
+			iManager.setColorManager(cManager);
+
+			int zoomMin = PropertiesManager.getInstance().getPropertiesAsInt("zoom.min");
+			int zoomMax = PropertiesManager.getInstance().getPropertiesAsInt("zoom.max");
+			while (zoomMin <= zoomMax) {
+				tManager.cutRaster(raster, zoomMin);
+				zoomMin++;
+			}
+		
+			}
+		}
+		
+		
 	}
 	
 }
