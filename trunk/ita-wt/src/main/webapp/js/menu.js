be91bbd1b23654639dc127494481076fa2c8702e
jhoanse <jhoanse@ffa86d28-1ee6-fafb-8203-f7d4c9a408cd>
2011-03-15 20:13:21 +0000

    organizing specie section in principal menu to read from menu.xml file. (Not Finished yet)

--- trunk/ita-wt/src/main/webapp/js/menu.js
+++ trunk/ita-wt/src/main/webapp/js/menu.js
@@ -29,7 +29,8 @@ function menu() {
 				document.write(items[j].childNodes[0].nodeValue);
 				document.write("</label></li></h5><br />");
 			}
-		}else if(categories[i].attributes.getNamedItem("name").nodeValue=="Bioclim"){	
+		}
+		if(categories[i].attributes.getNamedItem("name").nodeValue=="Bioclim"){	
 			var items = categories[i].getElementsByTagName("item");
 			var j;
 			for(j = 0; j < items.length; j++) {			
@@ -41,7 +42,8 @@ function menu() {
 				document.write(items[j].childNodes[0].nodeValue);
 				document.write("</label></li></h5><br />");
 			}
-		}else if(categories[i].attributes.getNamedItem("name").nodeValue=="Species"){	
+		}
+		if(categories[i].attributes.getNamedItem("name").nodeValue=="Species"){	
 			var items = categories[i].getElementsByTagName("item");
 			var j;
 			for(j = 0; j < items.length; j++) {			
@@ -53,7 +55,8 @@ function menu() {
 				document.write(items[j].childNodes[0].nodeValue);
 				document.write("</label></li></h5><br />");
 			}
-		}if(categories[i].attributes.getNamedItem("name").nodeValue=="Summaries"){	
+		}
+		if(categories[i].attributes.getNamedItem("name").nodeValue=="Summaries"){	
 			var items = categories[i].getElementsByTagName("item");
 			var j;
 			for(j = 0; j < items.length; j++) {			
@@ -65,6 +68,46 @@ function menu() {
 				document.write(items[j].childNodes[0].nodeValue);
 				document.write("</label></li></h5><br />");
 			}
+		} 
+		if(categories[i].attributes.getNamedItem("name").nodeValue=="Species"){
+			alert('id');
+			document.write("<div id=button><input type=button value='Search Species' ></div>");
+	        document.write("<div id=infoEspecie style='display:none;'>");
+	        document.write("<h5><label> class :</label><label id=data> mammalia</label></h5>");     
+	        document.write("<h5><label> family : Muridae</label></h5>");    
+	        document.write("<h5><label> genus : Nectomys</label></h5>");    
+	        document.write("<h5><label> specie : Nectomys apicalis</label></h5>");  
+	        document.write("<h5><input type=checkbox name='occurrences' key='' class=specieData> Occurrences records</h5>");        
+	        document.write("<h5><input type=checkbox name='convex' key='' class=specieData> Convex hull</h5>");
+	        document.write("<h5><input type=checkbox name='convexHull' key='' class=specieData> Convex hull buffer</h5>");
+	        document.write("<h5><input type=checkbox id='0' name='distributionLimitedtoconvex' key='' class=specieDistribution> Species distribution limited to convex hull (probabilities)</h5>");
+	        document.write("<h5><input type=checkbox id='1' name='distribution' key='' class=specieDistribution >Species distribution (probabilities)</h5>");
+	        document.write("<h5><input type=checkbox id='2' name='threshold' key='' class=specieDistribution >Thresholded species distribution</h5>");
+	        document.write("</div>");
+			document.write("<div id=popupContact>");
+	        document.write("<a id=popupContactClose>x</a>");        
+	        document.write("<h1>Taxononomic Species Menu</h1>");
+	        document.write("<p id=contactArea>");
+	        document.write("<div class='searchSpecie'>");
+	        document.write("<input id='search' class='ui-widget' /><input type=button value=Search />");
+	        document.write("</div>");
+	        document.write("<div id='treeStyle'>");
+	        document.write("<ul class='general'>");
+	        document.write("<li id='13140804' rank='1000' class='element'><a href=''> Plants</a></li>");
+	        document.write("<br>");
+	        document.write("<li id='13140952' rank='3000' class='element'><a href=''> Amphibians</a></li>");
+	        document.write("<br>");
+	        document.write("<li id='13140955' rank='3000' class='element'><a href=''> Birds</a></li>");
+	        document.write("<br>");
+	        document.write("<li id='13140937' rank='3000' class='element'><a href=''> Insects</a></li>");
+	        document.write("<br>");
+	        document.write("<li id='13140957' rank='3000' class='element'><a href=''> Mammals</a></li>");
+	        document.write("<br>");
+	        document.write("<li id='13140958' rank='3000' class='element'><a href=''> Reptiles</a></li>");
+	        document.write("</ul>");
+	        document.write("</div>");
+	        document.write("</p></div>");
+	        document.write("<div id=backgroundPopup></div>");			
 		}
 		document.write("</div></ul></li>");
 	}
