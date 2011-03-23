14402d7c02243c0ff379abae657b4eefa86f9c50
jhoanse <jhoanse@gmail.com>
2011-03-23 15:13:08 +0000

    put threat path into info class.

--- ita-wt/src/main/java/org/ciat/ita/service/Info.java
+++ ita-wt/src/main/java/org/ciat/ita/service/Info.java
@@ -7,6 +7,7 @@ public class Info {
 	private static String ip = "gisbif.ciat.cgiar.org";
 	private static String port = "3306";
 	private static String database = "gbif_sept2010";
+	private static String path = "";
 	
 	
 	public static String getUser() {
@@ -24,6 +25,12 @@ public class Info {
 	public static String getDatabase() {
 		return database;
 	}
-	
+	public static String getPath() {
+		if(System.getProperty("os.name").toLowerCase().contains("windows")) {
+			return "\\\\172.22.33.85\\geodata\\Threat-Assement\\species\\";
+		} else {
+			return "/mnt/HD724-geodata/iabin-threats/ITA/generated-files/species/";
+		}
+	}	
 	
 }
