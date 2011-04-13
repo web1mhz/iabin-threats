48a6938144d00ca18fcceade13d7ffff791091eb
jhoanse <jhoanse@ffa86d28-1ee6-fafb-8203-f7d4c9a408cd>
2011-04-13 23:24:32 +0000

    implementing scale functionality for all menu sections. (There are still some bugs appearing)

--- trunk/ita-wt/src/main/webapp/js/menu.js
+++ trunk/ita-wt/src/main/webapp/js/menu.js
@@ -107,17 +107,20 @@ function menu(){
             document.write("<div id=specieButtons>");
             document.write("<a href='' id=searchSpeciePopupButton class=utilityButtons>Search</a>");
             document.write("<div id=showSpeciesInfo style=display:none><a href='' class=utilityButtons>Show Info</a></div>");
-            document.write("<a href='' class=utilityButtons id=clear" + categories[i].attributes.getNamedItem("id").nodeValue + " style=display:none> Clear </a>");
-            document.write("<div id='opacitySpecie' class='opacityBar' style='display:none;'></div>");
+            document.write("<a href='' class=utilityButtons id=clear" + categories[i].attributes.getNamedItem("id").nodeValue + " style=display:none> Clear </a>");           
             document.write("</div>");
             document.write("<br /><hr class='submenuDivision'>");
-            document.write("<div id=infoSpecieOptions class=submenuLayout style='display:none;'>");
-            document.write("<input type=checkbox id='ocurrences' name='occurrences' key='' class=specieData> Occurrences records<br>");
-            document.write("<input type=checkbox id='convex' name='convex' key='' class=specieData> Convex hull<br>");
-            document.write("<input type=checkbox id='convexHull' name='convexHull' key='' class=specieData> Convex hull buffer<br>");
-            document.write("<input type=checkbox id='0' name='dist_limited_to_convex_hull' key='' class=specieDistribution> Species distribution limited to convex hull (probabilities)<br>");
-            document.write("<input type=checkbox id='1' name='full' key='' class=specieDistribution >Species distribution (probabilities)<br>");
-            document.write("<input type=checkbox id='2' name='full_with_threshold' key='' class=specieDistribution >Thresholded species distribution<br>");
+            document.write("<div id=infoSpecieOptions class=submenuLayout style='display:none;'>");			
+            document.write("<li><input type=checkbox id='ocurrences' name='occurrences' key='' class=specieData> Occurrences records</li>");
+            document.write("<li><input type=checkbox id='convex' name='convex' key='' class=specieData> Convex hull</li>");
+            document.write("<li><input type=checkbox id='convexHull' name='convexHull' key='' class=specieData> Convex hull buffer</li>");
+            document.write("<li><input type=checkbox id='0' name='dist_limited_to_convex_hull' key='' class=specieDistribution> Species distribution limited to convex hull (probabilities)");
+			document.write(" <img class='showScaleOpacity' src='images/minus.gif' style='display: none;'/> </li>");
+            document.write("<li><input type=checkbox id='1' name='full' key='' class=specieDistribution >Species distribution (probabilities)");
+			document.write(" <img class='showScaleOpacity' src='images/minus.gif' style='display: none;'/> </li>");
+            document.write("<li><input type=checkbox id='2' name='full_with_threshold' key='' class=specieDistribution >Thresholded species distribution");
+			document.write(" <img class='showScaleOpacity' src='images/minus.gif' style='display: none;'/></li>");
+			
             document.write("</div>");
         }
         document.write("</div></ul></li>");
